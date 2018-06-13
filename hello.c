95f85a0335c8aca8a79fb8eca27e3b021eeaf5b0
SeongJae Park <sj38.park@gmail.com>
2018-06-13 13:09:43 +0900

    Add a code for cpu time accounting
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- hello.c
+++ hello.c
@@ -2,6 +2,30 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+/*
+ * Count cpu time of this kernel thread.
+ *
+ * NOTE: CONFIG_TICK_CPU_ACCOUNTING should be ON
+ */
+#include <linux/sched.h>
+
+void count_cputime(void)
+{
+	unsigned long i;
+	volatile unsigned long sum;
+
+	pr_info("sj: Hello world\n");
+	pr_info("sj: [before] utime %llu, stime %llu, gtime %llu\n",
+			current->utime, current->stime, current->gtime);
+
+	for (i = 0, sum = 0; i < 500000000; i++)
+		sum += i;
+
+	pr_info("sj: sum is %lu\n", sum);
+	pr_info("sj: [after] utime %llu, stime %llu, gtime %llu\n",
+			current->utime, current->stime, current->gtime);
+}
+
 static int hello_init(void)
 {
 	pr_info("sj: Hello world\n");
