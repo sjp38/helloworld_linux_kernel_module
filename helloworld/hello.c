03cfeb4a10063df5e1a4ac36a7dd4fa5f0b09aab
SeongJae Park <sj38.park@gmail.com>
2019-03-23 09:13:36 +0900

    helloworld: Use 'pr_fmt()' macro
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -1,18 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#define pr_fmt(fmt) "hello: " fmt
+
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 
 static int __init hello_init(void)
 {
-	pr_info("sj: Hello world\n");
+	pr_info("Hello world\n");
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_info("sj: Goodbye world\n");
+	pr_info("Goodbye world\n");
 }
 
 module_init(hello_init);
