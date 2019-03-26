502f5cfce69a1491049af8f6d945ea1d8ef81182
SeongJae Park <sj38.park@gmail.com>
2019-03-26 16:22:46 +0900

    pr_vma: Restore pr_vma function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- experimental/pr_vma/pr_vma.c
+++ experimental/pr_vma/pr_vma.c
@@ -53,6 +53,29 @@ static void validate_vmas_order(void)
 	}
 }
 
+static void pr_vmas(void)
+{
+	struct task_struct *task;
+	struct vm_area_struct *vma;
+	struct mm_struct *mm;
+
+	for_each_process(task) {
+		pr_info("%s [%d]\n", task->comm, task->pid);
+		mm = task->mm;
+		if (!mm)
+			continue;
+		down_read(&mm->mmap_sem);
+		vma = mm->mmap;
+
+		while (vma) {
+			pr_info("%012lx-%012lx", vma->vm_start, vma->vm_end);
+			vma = vma->vm_next;
+		}
+		up_read(&mm->mmap_sem);
+		pr_info("\n");
+	}
+}
+
 static int __init pr_vma_init(void)
 {
 	pr_info("init\n");
