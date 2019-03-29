3f40e2a60e31217009e2799d28ee05e315e64115
SeongJae Park <sj38.park@gmail.com>
2019-03-30 08:13:42 +0900

    fileio: Implement file io function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- experimental/fileio/fileio.c
+++ experimental/fileio/fileio.c
@@ -2,13 +2,44 @@
 
 #define pr_fmt(fmt) "fileio: " fmt
 
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+static void fileio_example(void)
+{
+	struct file *f;
+	char buffer[256];
+	ssize_t sz;
+	loff_t pos;
+
+	f = filp_open("/hello_lkm_test", O_CREAT | O_RDWR, 0600);
+	if (IS_ERR(f)) {
+		pr_err("Failed to open file\n");
+		return;
+	}
+
+	sz = kernel_read(f, buffer, 256, &pos);
+	if (sz == 0)
+		pr_info("nothing read\n");
+	else
+		pr_info("read %s\n", buffer);
+
+	memcpy(buffer, "hello fileio\n", 256);
+	sz = kernel_write(f, buffer, strnlen(buffer, 256), &pos);
+	if (sz != strnlen(buffer, 256))
+		pr_info("failed to write the string\n");
+	else
+		pr_info("written the string %s\n", buffer);
+
+	filp_close(f, NULL);
+}
+
 static int __init fileio_init(void)
 {
 	pr_info("init\n");
+	fileio_example();
 	return 0;
 }
 
